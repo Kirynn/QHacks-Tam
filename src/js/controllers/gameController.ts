1dcfc282b13a9ac47bd67315aabf858c7f9bea2e
theasianmatt <60231177+theasianmatt@users.noreply.github.com>
2020-01-31 22:45:01 -0500

    updated gameController

--- src/js/controllers/gameController.ts
+++ src/js/controllers/gameController.ts
@@ -1,4 +1,5 @@
 import {tamController} from "./tamController";
+import {animController} from "./animController";
 
 export class gameController {
 
@@ -14,5 +15,25 @@ export class gameController {
 
     updateTam() {
         this.tams[0].updateHealth(10);
+        this.tams[0].updateHappiness(10);
+    }
+
+    moveTam() {
+        this.tams[0].moveLeft(5);
+        this.tams[0].moveRight(5);
+        this.tams[0].jump(5);
+        this.tams[0].crouch(5);
+    }
+
+    updateStateTam() {
+
+    }
+
+    deceaseTam(){
+        this.tams.shift();
+    }
+
+    nameTam(){
+        this.tams[0].updateName(name);
     }
 }
