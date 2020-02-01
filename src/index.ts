ef7a6c1a0b8d2cc2305668cac72eca777531c572
catherinewu12 <wucatherine12@gmail.com>
2020-01-31 23:38:32 -0500

    adding canvas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import { tamController } from './js/controllers/tamController'
 
-console.log("working");
+
+function canvas () {
+    var canvas: any = document.querySelector('canvas');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    var c = canvas.getContext('2d');
+
+    //test
+    c.beginPath(); //start line
+    c.moveTo(50, 300); 
+    c.lineTo(300, 100);
+    c.strokeStyle = "rgba(255, 0, 0, 0.5)";
+    c.stroke();   //actually get line
+
+
+}
+
+canvas();
+
+console.log("hi");
