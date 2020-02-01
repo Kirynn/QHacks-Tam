5b189d910402bf44ab08aa835dbcf3f9ffaac319
Kirynn <kieran1200@gmail.com>
2020-01-31 21:40:21 -0500

    Example Stuff

--- src/js/controllers/gameController.ts
+++ src/js/controllers/gameController.ts
@@ -1,6 +1,18 @@
+import {tamController} from "./tamController";
+
 export class gameController {
+
+    tams: Array<tamController> = [];
 
     constructor() {
 
+    }
+
+    addTam(tam: tamController) {
+        this.tams.push(tam);
+    }
+
+    updateTam() {
+        this.tams[0].updateHealth(10);
     }
 }
--- src/js/controllers/tamController.ts
+++ src/js/controllers/tamController.ts
@@ -1,8 +1,16 @@
+import {gameController} from "./gameController";
+
 export class tamController {
 
     health: Number = 0;
+    animcntrl: gameController;
+    
 
     constructor() {
 
+    }
+
+    updateHealth(i:number) {
+        this.health = i;
     }
 }
