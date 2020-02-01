7fc746d1210da4d8cb253ea49b6aadf06a651a32
catherinewu12 <wucatherine12@gmail.com>
2020-02-01 02:16:52 -0500

    drawing image

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,28 @@ import testchar from '@assets/testchar.json';
 
 let animCtrl : animController = new animController(testchar);
 
-console.log("trying to add image");
 
-let el = document.createElement('img');
-el.src = animCtrl.sheet.src;
-
-document.getElementById("app")?.appendChild(el);
 
 function canvas () {
     var canvas: any = document.querySelector('canvas');
-    var image: any = new Image();
-
-
-    image.src = 'the-scream.svg';
     
-    //document.getElementById('scream');
-
+    //load image
+    let el = document.createElement('img');
+    el.src = animCtrl.sheet.src;
+    
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     
     var c = canvas.getContext('2d');
 
-    //test
-    c.beginPath(); //start line
-    c.moveTo(50, 300); 
-    c.lineTo(300, 100);
-    c.strokeStyle = "rgba(255, 0, 0, 0.5)";
-    c.stroke();   //actually get line
+    c.fillText("My Tamagachi", 540,280);
 
-    c.fillText("Tamagachi", 500,200);
-
-    image.onload = function() {
-        c.drawImage(image, 10, 10, 200, 300);
+    //draw image on canvas
+    el.onload = function() {
+        c.drawImage(el, 0,0, 70, 100, 500, 20, 150, 250); //cutting the image into different pieces
+        //open image in paint and figure out actual dimensions
     }
     
-
 }
 
 canvas();
