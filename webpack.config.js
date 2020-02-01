399e812e41ff107ccf7a88a1bbacae80427d4efb
Kirynn <kieran1200@gmail.com>
2020-01-31 20:55:12 -0500

    Init

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    // Change to your "entry-point".
+    entry: './src/index',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            // Include ts, tsx, js, and jsx files.
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        },
+        {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: "style-loader"
+                },
+                {
+                    loader: "css-loader"
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        ident: "postcss",
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+        },
+        {
+            // Loads the javacript into html template provided.
+            // Entry point is set below in HtmlWebPackPlugin in Plugins 
+            test: /\.html$/,
+            use: [{loader: "html-loader"}]
+          }],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: "./src/html/index.html",
+          filename: "./index.html",
+          excludeChunks: [ 'server' ]
+        })
+      ]
+};
